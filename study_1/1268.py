75a6720944254553e44d524e7b03f04f0da16e9d
yeonju501 <loling970124@gmail.com>
2021-09-21 02:50:34 +0900

    0920

--- study_1/1268.py
+++ study_1/1268.py
@@ -1,25 +1,24 @@
 # 학생수대로 리스트 받아서 세어준다
 import sys
-x = int(sys.stdin.readline())
-cls = []
-for _ in range(x):
-    cls.append(list(map(int, sys.stdin.readline().strip().split(' '))))
-stu = [[0]*len(cls) for _ in range(5)]
-for i in range(5):
-    for j in range(len(cls)):
-        stu[i][j] = cls[j][i]
-m = 0
-m_stu = 0
-for i in range(len(cls)): # 사람수
-    cnt = 0
-    for j in range(len(cls[i])): # 반 번호
-        cnt += stu[j].count(cls[i][j])-1
-    if cnt > m:
-        m = cnt
-        m_stu = i
-    elif cnt == m:
-        continue
-print(m_stu+1)
+sys.stdin = open('1268_input.txt')
+# input = sys.stdin.readline
+T = int(input())
+l = [[0] * T for _ in range(T)]
+a = []
+arr = [list(map(int, input().split())) for _ in range(T)]
+for i in range(T):
+    for j in range(5):
+        for z in range(T):
+            if arr[i][j] == arr[z][j]:
+                l[i][z] = 1
+
+for i in range(T):
+    s = l[i].count(1)
+    a.append(s)
+
+m = max(a)
+print(a.index(m)+1)
+
 
 
         
